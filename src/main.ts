8f8b40fb6e7b781f226dc149e5954abd2c3d2946
dattrinh09 <tiendattrinh09@gmail.com>
2023-07-04 00:37:02 +0700

    Fix cors 5

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   app.enableCors({
     origin: 'http://localhost:3000',
     methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS',
-    credentials: false,
+    credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.setGlobalPrefix('/api');
