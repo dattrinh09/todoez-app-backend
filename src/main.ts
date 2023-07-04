dbdb0d866bbd0fc78ee0d6927f31fee1d5d2e7fd
dattrinh09 <tiendattrinh09@gmail.com>
2023-07-04 21:10:14 +0700

    Fix cors 7

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,6 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.enableCors({
-    origin: 'http://localhost:3000',
-    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS',
-    credentials: true,
-  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.setGlobalPrefix('/api');
   await app.listen(8080);
